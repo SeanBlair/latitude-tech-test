3b37110247976429fab5b9db0db6c0cd205a56c9
Sean Blair <seandibango@gmail.com>
2018-06-17 21:34:09 -0700

    Clean up

--- SeanBlair/TerrainElevationPathClient.cs
+++ SeanBlair/TerrainElevationPathClient.cs
@@ -3,21 +3,25 @@ using System.IO;
 
 namespace SeanBlair
 {
-    class TerrainElevationPathClient
+  // Console application that demonstrates the functionality of the TerrainElevationPath class.
+  // Requires files N48W124.hgt and N49W124.hgt in the working directory.
+  class TerrainElevationPathClient
+  {
+    // A container to store a LatLon object and the name of its location.
+    private struct Location
     {
-        private struct Location
-        {
-            public string Name { get; }
-            public LatLon LatLon { get; }
-            public Location(string name, LatLon latLon)
-            {
-                this.Name = name;
-                this.LatLon = latLon;
-            }
-        }
-        static void Main(string[] args)
-        {
-            var testLocations = new Location[] {
+      public string Name { get; }
+      public LatLon LatLon { get; }
+      public Location(string name, LatLon latLon)
+      {
+        this.Name = name;
+        this.LatLon = latLon;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      var testLocations = new Location[] {
                 new Location("Victoria Harbour", new LatLon(48.424236, -123.383191)),
                 new Location("Victoria Downtown", new LatLon(48.431202, -123.355085)),
                 new Location("Mount Douglas", new LatLon(48.493261, -123.344507)),
@@ -30,33 +34,33 @@ namespace SeanBlair
                 new Location("Howe Sound", new LatLon(49.684024, -123.167445)),
                 new Location("Squamish", new LatLon(49.700848, -123.156063))
             };
-            var flightPath = new LatLon[testLocations.Length];
-            for (var i = 0; i < testLocations.Length; i++)
-            {
-                flightPath[i] = testLocations[i].LatLon;
-            }
+      var flightPath = new LatLon[testLocations.Length];
+      for (var i = 0; i < testLocations.Length; i++)
+      {
+        flightPath[i] = testLocations[i].LatLon;
+      }
+      var terrainElevationPath = new TerrainElevationPath(flightPath);
+      int[] elevationPath;
+      try
+      {
+        elevationPath = terrainElevationPath.GetElevationPath();
+      }
+      catch (Exception e) when (e is FileNotFoundException || e is ArgumentException)
+      {
+        Console.WriteLine(e.Message);
+        Console.WriteLine();
+        Console.WriteLine(e.StackTrace);
+        return;
+      }
 
-            var tep = new TerrainElevationPath(flightPath);
-            int[] elevationPath;
-            try
-            {
-                elevationPath = tep.GetElevationPath();
-            }
-            catch (Exception e) when (e is FileNotFoundException || e is ArgumentException)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine();
-                Console.WriteLine(e.StackTrace);
-                return;
-            }
-            
-            for (var i = 0; i < testLocations.Length; i++)
-            {
-                var location = testLocations[i];
-                Console.WriteLine($"Elevation at {location.Name} " +
-                    $"[{location.LatLon.Lat}, {location.LatLon.Lon}] is: " +
-                    $"{elevationPath[i]} meters.");
-            }
-        }
+      // Print the result to console.
+      for (var i = 0; i < testLocations.Length; i++)
+      {
+        var location = testLocations[i];
+        Console.WriteLine($"Elevation at {location.Name} " +
+            $"[{location.LatLon.Lat}, {location.LatLon.Lon}] is: " +
+            $"{elevationPath[i]} meters.");
+      }
     }
+  }
 }
